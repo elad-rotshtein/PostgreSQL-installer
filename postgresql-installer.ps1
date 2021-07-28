7f7a7e1fc16a79dbe49264ccb75e9308864abac5
elad.rotshtein <elad.rotshtein@gmail.com>
2021-07-29 02:46:17 +0300

    created PS script file

---
+++ postgresql-installer.ps1
@@ -0,0 +1,10 @@
+﻿#######################################
+#
+# Elad Rotshtein
+#
+# postgreqsl-installer-configurator
+#
+#######################################
+
+# URL for postgresql installer
+$downloadURL = 'https://www.enterprisedb.com/postgresql-tutorial-resources-training?cid=48'
