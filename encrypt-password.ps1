6526dca3f38630a66924dc878388c16a30f76a6b
elad.rotshtein <elad.rotshtein@gmail.com>
2021-07-30 04:44:31 +0300

    big progress on main script

--- encrypt-password.ps1
+++ encrypt-password.ps1
@@ -1,54 +1,59 @@
-﻿#########################################
-#                                       #
-# Encrypt-Password                      #
-#                                       #
-# Elad Rotshtein                        #
-#                                       #
-#########################################
+﻿##########################################
+#                                       
+# Encrypt-Password                                                                                
+#                                       
+##########################################
 
+# save your key and encrypted password only to folders with highly restricted access permissions!!!
 $keyDir = 'C:\encrypted_data'
 $keyFileName = 'key.txt'
 $PWDDir = $keyDir 
 $PWDFileName = 'crypt.txt'
 
-$keyFilePath = "$($keyDir + '\' + $keyFileName)"
-$PWDFilePath = "$($PWDDir + '\' + $PWDFileName)"
+$keyPath = "$($keyDir + '\' + $keyFileName)"
+$PWDPath = "$($PWDDir + '\' + $PWDFileName)"
 
 $PWDFileSuccess = $true
 
+try
+{
+    if (!(Test-Path $keyDir)) {
+        New-Item -ItemType Directory -Path $keyDir
+        write-host "Created folder at path $keyDir"
+    }
 
-if (!(Test-Path $keyDir)) {
-    New-Item -ItemType Directory -Path $keyDir
-    write-host "Created folder at path $keyDir"
+    $newKey = New-Object Byte[] 32
+    [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($newKey)
+
+    # createing a file to store the key
+    Set-Content -Path $keyPath -Value $newKey
+
+    # confrim creation of the key-containing file
+    if ("$(Get-Content $keyPath)" -eq "$newKey") {
+        Write-Host -ForegroundColor Green "Key file created successfully at $keyPath"
+    }
+    else {
+        Write-Warning "could'nt verify the creation of a valid key file at $keyPath"
+    }
+}
+finally
+{
+    Remove-Variable newKey
 }
 
-$newKey = New-Object Byte[] 32
-[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($newKey)
+# prompt user for password, convert the secure string to an encrypted standard string with with the new key and store it in a file
+$PWD = (Get-Credential -Message "Enter the password you wish to encrypt" -UserName "password only").Password | ConvertFrom-SecureString -key (get-content $keyPath)
+$PWD | Out-File $PWDPath
 
-# createing a file to store the key
-Set-Content -Path $keyFilePath -Value $newKey
-
-# confrim creation of the key-containing file
-if ("$(Get-Content $keyFilePath)" -eq "$newKey") {
-    Write-Host -ForegroundColor Green "Key file created successfully at $keyFilePath"
-}
-else {
-    Write-Warning "could'nt verify the creation of a valid key file at $keyFilePath"
-}
-
-# prompt user for password, convert the secure string to an encrypted standard string with with the new key and store in file
-$PWD = (Get-Credential -Message "Enter the password you wish to encrypt" -UserName "password only").Password | ConvertFrom-SecureString -key ($newKey)
-$PWD | Out-File $PWDFilePath
-
-# confrim creation of password-containing file
-if (!(Get-Content $PWDFilePath) -eq $PWD) {
+# confrim creation of the password-containing file
+if (!(Get-Content $PWDPath) -eq $PWD) {
     $PWDFileSuccess = $false
 }
 
-# confirm the password-containing file can be dexrypted
+# confirm the password-containing file can be decrypted
 try
 {
-    Get-Content $PWDFilePath | ConvertTo-SecureString -Key (get-content $keyFilePath) -ErrorAction stop | Out-Null
+    Get-Content $PWDPath | ConvertTo-SecureString -Key (get-content $keyPath) -ErrorAction stop | Out-Null
 }
 catch
 {
@@ -58,5 +63,5 @@ catch
 }
 
 if ($PWDFileSuccess) {
-    Write-Host -ForegroundColor Green "password file created successfully at $PWDFilePath"
+    Write-Host -ForegroundColor Green "password file created successfully at $PWDPath"
 }
--- postgresql-installer.ps1
+++ postgresql-installer.ps1
@@ -2,15 +2,72 @@
 #
 # PostgreSQL-InstallerConfigurator
 #
-# Elad Rotshtein
-#
 ######################################################
 
-# URL to download installer from
-$DLURL = 'https://www.enterprisedb.com/postgresql-tutorial-resources-training?cid=48'
-$DLDir = '.'
+$transcript = "PostgreSQL-Installer.log"
 
-$superusername = 'aidocapp'
+# path of key and encrypted password files
+$keyPath = 'C:\encrypted_data\key.txt'
+$PWDPath = 'C:\encrypted_data\crypt.txt'
 
-# need to implament more securely
-$superuserpass = 
+# function defenition
+
+function Install-PostgreSQL
+{
+    param (
+        [string]$Uri = 'https://www.enterprisedb.com/postgresql-tutorial-resources-training?cid=48',  
+        [string]$destination = 'PostgreSQL_Installer.exe',
+        [string]$superaccount='postgres',
+        [Parameter(mandatory)][string]$superpassword
+    )
+    
+    try
+    {
+    Invoke-WebRequest -Uri $Uri -OutFile $destination -ErrorAction stop
+    }
+    catch
+    {
+    throw "Error received while attempting to download installer from $uri. Error: $($error[0])"
+    }
+
+    try
+    {
+       Start-Process $destination -ArgumentList  "--mode unattended --unattendedmodeui none --superaccount $superaccount --superpassword $superpassword" -Wait -ErrorAction Stop
+    }
+    catch
+    {
+    throw "Error received while attempting to run the installer. Error: $($error[0])"
+    }
+}
+
+
+
+
+# main
+Start-Transcript -path $transcript
+
+try
+{
+
+    try
+    {
+    $superPassword = Get-Content $PwdPath | ConvertTo-SecureString -Key (get-content $keyPath)
+    }
+    catch
+    {
+    throw "Error received while attempting to get and decrypt the password. Error: $($error[0])"
+    }
+    
+    try
+    {
+        Install-PostgreSQL -superaccount 'aidocapp' -superpassword $superPassword
+    }
+    catch
+    {
+    throw "Error received while using Install-PostgreSQL. Error: $($error[0])"
+    }
+}
+finally
+{
+    Stop-Transcript
+}
